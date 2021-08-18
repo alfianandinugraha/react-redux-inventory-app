477127eb65c25d26362f1cfcbd5636f29c03341e
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-18 14:12:01 +0700

    feat(get products): get products from redux

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,23 +1,11 @@
-import { Product } from 'state'
-import { nanoid } from 'nanoid'
 import { Grid } from '@material-ui/core'
 import ProductItem from '@/components/ProductItem'
 import React from 'react'
-
-const productFactory = (id: string, name: string, stock: number): Product => ({
-  id,
-  name,
-  stock,
-})
+import { useSelector } from 'react-redux'
+import { Store } from 'store'
 
 const ProductList = () => {
-  const products: Product[] = [
-    productFactory(nanoid(), 'Asus MPM1', 5),
-    productFactory(nanoid(), 'Lenovo Thinkpad T440p', 10),
-    productFactory(nanoid(), 'Pen Pilot', 20),
-    productFactory(nanoid(), 'Book Sidu', 30),
-    productFactory(nanoid(), 'Lenovo Ideapad', 3),
-  ]
+  const products = useSelector((state: Store) => state.products)
 
   return (
     <Grid container spacing={3}>
