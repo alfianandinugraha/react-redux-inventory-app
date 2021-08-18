8d7ee84d7b6bbcfa541d2f521fd1058566fdd97f
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-18 12:51:43 +0700

    feat(input handling): add form input handling on ProductForm

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,21 +1,48 @@
 import { Button, Grid, TextField } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 
 const ProductForm = () => {
+  const [name, setName] = useState('')
+  const [stock, setStock] = useState('')
+
   return (
     <Grid container spacing={2} direction="column">
       <Grid item>
         <Grid container spacing={2}>
           <Grid item>
-            <TextField type="text" label="Product Name" />
+            <TextField
+              type="text"
+              label="Product Name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value)
+              }}
+            />
           </Grid>
           <Grid item>
-            <TextField type="number" label="Product Stock" />
+            <TextField
+              type="number"
+              label="Product Stock"
+              value={stock}
+              onChange={(e) => {
+                setStock(e.target.value)
+              }}
+            />
           </Grid>
         </Grid>
       </Grid>
       <Grid item>
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            const payload = {
+              name,
+              stock: +stock,
+            }
+            console.log(payload)
+          }}
+        >
           Add product
         </Button>
       </Grid>
